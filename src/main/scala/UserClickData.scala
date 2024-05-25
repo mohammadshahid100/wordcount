3ce91e0bad0dcc59145e800a9b0e61d2bcf3d57f
HP ELITEBOOK <mohammadshahidking@gmail.com>
2024-05-25 14:58:54 +0530

    finaloutput

--- src/main/scala/UserClickData.scala
+++ src/main/scala/UserClickData.scala
@@ -1,5 +1,4 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 
 object UserClickData extends App{
@@ -13,35 +12,16 @@ object UserClickData extends App{
   val jsonFilePath = "C:/Users/HP ELITEBOOK/Desktop/data.json"
   val rawDF = spark.read.json(jsonFilePath)
 
-  // Assuming a predefined schema
-  val schema = StructType(Seq(
-    StructField("click_event_id", StringType, nullable = true),
-    StructField("user_id", LongType, nullable = true),
-    StructField("url", StringType, nullable = true),
-    StructField("timestamp", TimestampType, nullable = true),
-    StructField("city", StringType, nullable = true),
-    StructField("country", StringType, nullable = true),
-    StructField("ip_address", StringType, nullable = true),
-    StructField("device", StringType, nullable = true),
-    StructField("browser", StringType, nullable = true)
-  ))
-
   // Apply schema and convert timestamp format
-  val rawDFWithSchema = spark.read.schema(schema).json(jsonFilePath)
+//  val rawDFWithSchema = spark.read.schema(schema).json(jsonFilePath).show()
 
-  rawDFWithSchema.withColumn("date", date_format(col("timestamp"), "yyyy-MM-dd") )
-
-  val outputDF = rawDFWithSchema
-    .groupBy("url", "country", "date")
+  val outputDF = rawDF
+    .groupBy(col("url"), col("country"), date_format(col("timestamp"), "yyyy-MM-dd")
+      .alias("event_date"))
     .agg(
-      avg(col("time_spent")).alias("avg_time_spent"),
-      countDistinct(col("user_id")).alias("unique_users"),
-      count(col("click_event_id")).alias("clicks")
+      avg(minute(col("timestamp")).cast("int")).alias("avg_minute_spent"),
+      countDistinct(col("user_id")).alias("unique_user_count"),
+      count(col("click_event_id")).alias("click_count")
     )
-  val outputPath = "C:/Users/HP ELITEBOOK/Desktop/output.csv"
-  outputDF
-    .write
-    .mode("overwrite")
-    .csv(outputPath)
-
+  outputDF.show()
 }
